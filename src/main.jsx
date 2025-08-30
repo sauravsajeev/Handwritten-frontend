4a53b99e3df89e978c5b18b0f9f0adbb67b2d915
Juzznick <sauravsajeev47@gmail.com>
2025-08-30 15:45:52 +0530

    Fix rollup build issuess

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 import reportWebVitals from "./reportWebVitals";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
