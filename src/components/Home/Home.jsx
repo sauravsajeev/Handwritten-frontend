090f40d131d8c074952a314452028c401a73c2be
Juzznick <sauravsajeev47@gmail.com>
2025-09-03 17:54:59 +0530

    preview image fix

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -330,7 +330,14 @@ function Home() {
     const rect = overlayRef.current.getBoundingClientRect()
     const w = e.clientX - rect.left - crop.x
     const h = e.clientY - rect.top - crop.y
+      const newCrop = { ...crop, w, h }
     setCrop({ ...crop, w, h })
+    const ctx = overlayRef.current.getContext("2d")
+  ctx.clearRect(0, 0, overlayRef.current.width, overlayRef.current.height)
+
+  ctx.strokeStyle = "red"   // ✅ rectangle color
+  ctx.lineWidth = 2
+  ctx.strokeRect(newCrop.x, newCrop.y, newCrop.w, newCrop.h)
   }
 
   const handleMouseUp = () => setIsDrawing(false)
